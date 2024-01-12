e753619f2957b1922999dafa7d3e3ccfbf52eb24
WadeWei0307 <a0926151140@gmail.com>
2024-01-12 21:44:42 +0800

    Time 0ms 100%, Space 6.65MB 18.50%

--- 0070-Climbing Stairs/0070-Climbing Stairs.cpp
+++ 0070-Climbing Stairs/0070-Climbing Stairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
- public:
-  int climbStairs(int n) {
-    if (n == 0 || n == 1) {
-      return 1;
+public:
+    int climbStairs(int n) {
+        vector<int> tempAns = {1,1};
+        if(n < 2)
+            return 1;
+        for(int i = 2; i <=n; i++){
+            tempAns.push_back(tempAns[i-1]+tempAns[i-2]);
+        }
+        return tempAns[tempAns.size()-1];
     }
-    vector<int> dp(n+1);
-    dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= n; i++) {
-      dp[i] = dp[i - 1] + dp[i - 2];
-    }
-    return dp[n];
-  }
 };
