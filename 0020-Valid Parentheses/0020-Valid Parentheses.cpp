d96d5f4f4497c8747998716d14fd06c5e2ccd1af
WadeWei0307 <a0926151140@gmail.com>
2024-01-12 18:38:50 +0800

    Time 0ms 100%, Space 6.32MB 100%

--- 0020-Valid Parentheses/0020-Valid Parentheses.cpp
+++ 0020-Valid Parentheses/0020-Valid Parentheses.cpp
@@ -1,62 +1,23 @@
-class Solution
-{
+class Solution {
 public:
-    bool isValid(string s)
-    {
-        int stack_top = 10;
-        char stack[10000];
-        int sum = 0;
-        for (int i = 0; i < s.size(); i++)
-        {
-            switch (s[i])
-            {
-            case '(':
-            case '[':
-            case '{':
-                stack[stack_top] = s[i];
-                stack_top++;
-                sum++;
-                break;
-            case ')':
-                if (stack[stack_top - 1] == '(')
-                {
-                    stack[stack_top] = '\0';
-                    stack_top--;
-                    sum--;
-                }
-                else
-                {
+    bool isValid(string s) {
+        stack<char> symbol;
+        map<char, char> m = {{'}','{'}, {']','['}, {')','('}};
+        for(int i=0; i<s.length(); i++){
+            if(!m.count(s[i])) //表示是向右的symbol
+                symbol.push(s[i]); //將向右的symbol推入stack
+            else{ //表示是向左的symbol
+                if(symbol.empty()) //若stakc為空，top()會error，所以一定得先判斷stack是否為空
                     return false;
-                }
-                break;
-            case ']':
-                if (stack[stack_top - 1] == '[')
-                {
-                    stack[stack_top] = '\0';
-                    stack_top--;
-                    sum--;
-                }
-                else
-                {
+                else if(symbol.top() == m[s[i]]) //若stack最上層的symbol跟hash table中的value一樣，表示valid
+                    symbol.pop();
+                else //若stack最上層的symbol跟hash table中的value不一樣，表示invalid
                     return false;
-                }
-                break;
-            case '}':
-                if (stack[stack_top - 1] == '{')
-                {
-                    stack[stack_top] = '\0';
-                    stack_top--;
-                    sum--;
-                }
-                else
-                {
-                    return false;
-                }
-                break;
             }
         }
-        if (sum != 0)
+        if(!symbol.empty())
             return false;
-        return true;
+        else
+            return true;
     }
 };
