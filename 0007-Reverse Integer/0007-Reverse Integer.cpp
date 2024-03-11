40708f269eacb179c3cb0b11f63711186d648a39
WadeWei0307 <a0926151140@gmail.com>
2024-03-11 21:00:21 +0800

    Time 0ms 100%, Space 7.40MB 44.52%

---
+++ 0007-Reverse Integer/0007-Reverse Integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int result = 0, digit = 0;
+        while(x != 0){
+            if(result > INT_MAX / 10 || result < INT_MIN / 10)
+                return 0;
+            result = result * 10 + x % 10;
+            x /= 10;
+        }
+        return result;
+    }
+};
